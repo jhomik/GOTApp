32e22fc988147ebca4ae49fa7d6370f5d4e8315d
Jakub <jh.homik@gmail.com>
2020-04-24 18:52:20 +0200

    adding userDefaults

---
+++ GoTApp/Custom Views/Cell/GoTCustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoTCustomCell.swift
+//  GoTApp
+//
+//  Created by Jakub Homik on 23/04/2020.
+//  Copyright © 2020 Jakub Homik. All rights reserved.
+//
+
+import UIKit
+
+class GoTCustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
